bd9c23f32f308f0b9dfe65016bd1f26dbd393eee
lukaokroshidze <lukaokroshidze9@gmail.com>
2024-10-09 10:17:40 -0700

    second commit

---
+++ day 06/classwork/classwork.py
@@ -0,0 +1,29 @@
+name = input("luka: ")
+surname = input("okroshidze: ")
+full_name = name + " " + surname
+print("luka okroshidze:", )
+
+
+=====================================
+
+name1 = float(input("10: "))
+name2 = float(input("25: "))
+operation = input( (+, -, *, /, //, **): ")
+                  
+
+ operation == '+':
+    result = name1 + name2
+ operation == '-':
+    result = name1 - name2
+ operation == '*':
+    result = name1 * name2
+ operation == '/':
+    result = name1 / name2
+ operation == '//':
+    result = name1 // name2
+ operation == '**':
+    result = name1 ** name2
+:
+    result = "არასწორი ოპერატორი"
+
+print("შედეგი:", result)
